c846933f1bede5daaa68c0ab0744895c96cdeed1
Anton Shpakovych <shpakovych18@gmail.com>
2023-07-18 20:24:56 +0300

    Implemented login

--- task_manager_service/urls.py
+++ task_manager_service/urls.py
@@ -6,6 +6,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("accounts/", include("django.contrib.auth.urls")),
     path("__debug__/", include("debug_toolbar.urls")),
     path("tasks/", include("task_manager.urls", namespace="task-manager"))
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
