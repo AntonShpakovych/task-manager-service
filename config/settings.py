471e3990fc1db6b2bf275649c486c871975aa36c
Anton Shpakovych <shpakovych18@gmail.com>
2023-07-24 17:26:20 +0300

    Try another way to set Debug=False on production

--- config/settings.py
+++ config/settings.py
@@ -8,7 +8,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 
-DEBUG = os.environ["DJANGO_DEBUG"]
+DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
 
 ALLOWED_HOSTS = ["127.0.0.1", "task-manager-stsc.onrender.com"]
 
